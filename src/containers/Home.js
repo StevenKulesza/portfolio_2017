601fe5f37b4d3c99a9dbcd7197c684269958cab4
Steven <info@stevenkulesza.com>
2017-09-17 18:02:29 -0400

    add assets and theme directories, import styled

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Image = styled.img`
+  width: 100%;
+`;
 
 export default class Home extends Component {
 
@@ -6,6 +11,10 @@ export default class Home extends Component {
     return (
       <div>
         <h1>Hi Home!</h1>
+        <Image src={require('../assets/test.jpg')} alt="test"/>
+        <p>
+        Lorem Ipsum
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
+import './theme/globalStyle';
+
 ReactDOM.render(
   routes,
   document.getElementById('root'));
